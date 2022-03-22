93187ea169b6be4a9646059336c533692b8e4c19
Andrea-gt <a.ximeram@gmail.com>
2022-03-21 21:49:09 -0600

    Evaluations with Maps

--- Controlador.java
+++ Controlador.java
@@ -6,10 +6,12 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
 public class Controlador {
 	FileInputStream fstream;
-	
+
 	public void readingFile(Map<String, List<String>> Map) throws IOException {
 
 		fstream = new FileInputStream("ListadoProducto.txt");
@@ -20,22 +22,38 @@ public class Controlador {
 		//Leer Archivo Linea por Linea
 		while ((strLine = br.readLine()) != null)   {
 			String[] parts = strLine.split(":");
-			
+
 			//Las partes del Array temporal se asignan como categoria y producto de la estructura
 			String category = parts[0].trim();
 			List<String> product = new ArrayList<>();
 			product.add(parts[1].trim());
-			
-			
+
+
 			if(Map.containsKey(category))
 				Map.get(category).add(parts[1].trim());
-			
+
 			else {
 				Map.put(category, product);
 			}
 		}
-		
+
 		//Cerrar el input
 		fstream.close();
+	}
+
+	public static void printMap(Map<String, List<String>> Map) {
+		for (Entry<String, List<String>> entry : Map.entrySet())
+			System.out.println("Categoria = " + entry.getKey() +
+					", Productos = " + entry.getValue());   
+	}
+
+	public static void sortbykey(Map<String, List<String>> Map)
+	{
+		TreeMap<String,List<String>> sorted = new TreeMap<>(Map);
+
+		// Display the TreeMap which is naturally sorted
+		for (Entry<String, List<String>> entry : sorted.entrySet())
+			System.out.println("Categoria = " + entry.getKey() +
+					", Productos = " + entry.getValue());       
 	}
 }
--- MainClass.java
+++ MainClass.java
@@ -8,22 +8,76 @@ public class MainClass {
 		Scanner sn = new Scanner(System.in); 
 		Controlador ctr = new Controlador();
 		MapFactory factory = new MapFactory();
-		
-		System.out.println("Bienvenido, por favor ingrese qué tipo de estructura desea ultizar");
+		boolean flag =true;
+
+		System.out.println("Bienvenido, por favor ingrese que tipo de estructura desea ultizar");
 		System.out.println("1. HashMap");
 		System.out.println("2. TreeMap");
 		System.out.println("3. LinkedHashMap");
-		
+
 		int selection = sn.nextInt();
 		sn.nextLine();
-		
+
 		Map<String, List<String>> Map = factory.createMap(selection);
-		 
+
 		try {
 			ctr.readingFile(Map);
-			System.out.println(Map);
-			
+
+			while(flag) {
+				System.out.println("\nQue desea hacer");
+				System.out.println("1. Agregar un Producto a la ColecciÃ³n");
+				System.out.println("2. Categoria del Producto");
+				System.out.println("3. Datos de Producto");
+				System.out.println("4. Datos de Producto Ordenados");
+				System.out.println("5. Categoria y Producto de Todo Inventario");
+				System.out.println("6. Categoria y Producto de Todo Inventario de Manera Ordenada\n");
+
+				int option = sn.nextInt();
+				sn.nextLine();
+
+				switch(option) {
+
+				case 1:
+					System.out.println("Ingrese a que categoria desea agregar");
+					String category = sn.nextLine();
+
+					if(Map.containsKey(category)) {
+						System.out.println("Ingrese los productos separados por una coma");
+						String productos = sn.nextLine();
+						String[] productosLista = productos.split(",");
+
+						for (int i=0; i<productosLista.length; i++) 
+						{ 
+							Map.get(category).add(productosLista[i]);
+						}
+					}
+
+					else {
+						System.out.println("No existe la categoria");
+					}
+
+					break;
+
+				case 2:
+					System.out.println("Ingrese el nombre del producto");
+					String productName = sn.nextLine();
+
+				case 3:
+
+				case 4:
+
+				case 5:
+					ctr.printMap(Map);
+					break;
+
+				case 6:
+					ctr.sortbykey(Map);
+					break;
+
+				}
+			}
 		} catch (IOException e) {
 		}
 	}
 }
+
